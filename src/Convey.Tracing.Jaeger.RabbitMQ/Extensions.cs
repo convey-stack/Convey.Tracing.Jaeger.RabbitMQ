d7c6975611da69773d345ea8a3f270cdf1f9c8a6
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-05 22:03:44 +0200

    RabbitMQ native

--- src/Convey.Tracing.Jaeger.RabbitMQ/Extensions.cs
+++ src/Convey.Tracing.Jaeger.RabbitMQ/Extensions.cs
@@ -1,18 +1,17 @@
 using Convey.MessageBrokers.RabbitMQ;
 using Convey.Tracing.Jaeger.RabbitMQ.Middlewares;
 using Microsoft.Extensions.DependencyInjection;
-using OpenTracing;
 
 namespace Convey.Tracing.Jaeger.RabbitMQ
 {
     public static class Extensions
     {
-        public static IRabbitMqPluginRegister RegisterJaeger(this IRabbitMqPluginRegister register)
+
+        public static IConveyBuilder AddJaegerRabbitMqMiddleware(this IConveyBuilder builder)
         {
-            var tracer = register.ServiceProvider.GetService<ITracer>();
-            register.AddPlugin(p => p.Register(pipe => pipe.Use<JaegerStagedMiddleware>(tracer)),
-                ioc => ioc.AddSingleton(tracer));
-            return register;
+            builder.Services.AddTransient<IRabbitMqMiddleware, JaegerMiddleware>();
+
+            return builder;
         }
     }
 }
